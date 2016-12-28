e7879bda023d747af805efc017c2b81af7ed126f
krishkribo <Krishna2.14mc@kct.ac.in>
2016-12-28 16:42:56 +0530

    Add files via upload

---
+++ client server_2.ino
@@ -0,0 +1,85 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+
+
+
+
+
+#define DEBUG true
+
+
+
+const char* ssid     = "AndroidAP";  
+const char* password = "rasegan";
+
+const char* host     = "kribo 001"; // Your domain  
+String path          = "A";  
+const int pin        = 8;
+
+void setup() {  
+  pinMode(pin, OUTPUT); 
+  pinMode(pin, HIGH);
+  Serial.begin(115200);
+
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid);
+  int wifi_ctr = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: " + WiFi.localIP());
+}
+
+void loop() {  
+  Serial.print("connecting to ");
+  Serial.println(1);
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(1, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  client.print(String("GET ") + path + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: keep-alive\r\n\r\n");
+
+  delay(500); // wait for server to respond
+
+  // read response
+  String section="header";
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    // Serial.print(line);
+
+    
+    // we’ll parse the HTML body here
+    if (section=="header") { // headers..
+      Serial.print(".");
+      if (line=="\n") { // skips the empty space at the beginning 
+        section="json";
+      }
+    }
+   
+
+      // Make the decision to turn off or on the LED
+      if (path='a') {
+        digitalWrite(pin, HIGH); 
+        Serial.println("LED ON");
+      }
+      else {
+        digitalWrite(pin, LOW);
+        Serial.println("led off");
+      }
+    }
+  } 
+
